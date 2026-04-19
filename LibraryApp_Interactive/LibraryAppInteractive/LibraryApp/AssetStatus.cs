02a21fe6e0293046d3de7882b87d7e19b9252104
Sean Roscoe <roscoes@shernet.sheridancollege.ca>
2026-04-19 17:09:20 -0400

    created all classes, structs and enum types

---
+++ LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/AssetStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryAppInteractive;
+
+public enum AssetStatus
+{
+    NotAvailable = 0,
+    Available = 1,
+    Loaned = 2,
+    Reserved = 3
+}
---
+++ LibraryApp_Interactive/LibraryAppInteractive/LibraryApp/LoanPeriod.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryAppInteractive;
+
+public struct LoanPeriod
+{
+    private DateTime _borrowedOn;
+    private DateTime _returnedOn;
+    private DateTime _dueDate;
+
+    public DateTime BorrowedOn
+    {
+        get { return _borrowedOn; }
+        set { _borrowedOn = value; }
+    }
+
+    public DateTime ReturnedOn
+    {
+        get { return _returnedOn; }
+        set { _returnedOn = value; }
+    }
+
+    public DateTime DueDate
+    {
+        get { return _dueDate; }
+        set { _dueDate = value; }
+    }
+
+    public TimeSpan LatePeriod
+    {
+        get
+        {
+            if (_returnedOn > _dueDate)
+                return _returnedOn - _dueDate;
+            else
+                return TimeSpan.Zero;
+        }
+    }
+
+    public LoanPeriod(DateTime borrowedOn, DateTime returnedOn)
+    {
+        _borrowedOn = borrowedOn;
+        _returnedOn = returnedOn;
+
+        _dueDate = borrowedOn.AddDays(14);
+    }
+}
